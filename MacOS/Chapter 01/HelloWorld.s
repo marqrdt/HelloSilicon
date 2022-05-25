409bfc43ba2ede8381309d2b8101df3926b1611a
Paul Marquardt <marqrdt@gmail.com>
2022-05-24 22:01:00 -0600

    Break project into MacOS and Linux.

--- MacOS/Chapter 01/HelloWorld.s
+++ MacOS/Chapter 01/HelloWorld.s
@@ -11,17 +11,18 @@
 
 // Setup the parameters to print hello world
 // and then call the Kernel to do it.
-_start: mov	X0, #1		// 1 = StdOut
+_start: mov	W0, #1		// 1 = StdOut
 	adr	X1, helloworld 	// string to print
-	mov	X2, #13	    	// length of our string
-	mov	X16, #4		// Unix write system call
+	//mov X3, #0x11
+	mov	W2, #0x11	    	// length of our string
+	mov	W16, #4		// Unix write system call
 	svc	#0x80		// Call kernel to output the string
 
 // Setup the parameters to exit the program
 // and then call the kernel to do it.
-	mov     X0, #0		// Use 0 return code
-	mov     X16, #1		// System call number 1 terminates this program
+	mov     W0, #0		// Use 0 return code
+	mov     W16, #1		// System call number 1 terminates this program
 	svc     #0x80		// Call kernel to terminate the program
 
-helloworld:      .ascii  "Hello World!\n"
+helloworld:      .ascii  "Hello Schnauzer!\n"
 
